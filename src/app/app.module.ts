6a80efe04f6e2b636e3bb69fa5558508515caed2
devopslayer <layerdevops@gmail.com>
2024-02-22 19:04:24 +0530

    Created Planets Component and API Service. And changed some pre styling and elements in App Component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanetsComponent } from './components/planets/planets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsComponent
   ],
   imports: [
     BrowserModule,
